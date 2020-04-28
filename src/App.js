cce22a80a0d8383ea159b4766743fa86eacd9ca5
garder77 <2635204377@qq.com>
2020-04-28 18:34:04 +0800

    hls流媒体播放

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import VideoContainer from './view/video'
+import React from 'react'
+// import VideoContainer from './view/video'
 // import VideoDplayer from './view/rdplayer'
 import DisPlayer from './view/dplayer'
-import UploadFiles from './view/uploadFiles'
+// import UploadFiles from './view/uploadFiles'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <VideoContainer />
+      {/* <VideoContainer /> */}
       {/* <VideoDplayer /> */}
       <DisPlayer />
-      <UploadFiles />
+      {/* <UploadFiles /> */}
     </div>
   );
 }
--- src/view/video/videoPlayer.jsx
+++ src/view/video/videoPlayer.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import videojs from 'video.js'
+// import "videojs-contrib-hls"
 import 'video.js/dist/video-js.css'
 
 class VideoPlayer extends React.Component {
-  screenshot() {
-    let root = document.getElementById('root')
-    let canvas = document.getElementById('videoCanvasBox');
-    canvas.width = this.videoNode.videoWidth;
-    canvas.height = this.videoNode.videoHeight;
-    canvas.getContext('2d').drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
-  }
+  // screenshot() {
+  //   let root = document.getElementById('root')
+  //   let canvas = document.getElementById('videoCanvasBox');
+  //   canvas.width = this.videoNode.videoWidth;
+  //   canvas.height = this.videoNode.videoHeight;
+  //   canvas.getContext('2d').drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
+  // }
   componentDidMount() {
     this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
       console.log('onPlayerReady', this)
     });
+
   }
   componentWillUnmount() {
     if (this.player) {
@@ -27,8 +29,8 @@ class VideoPlayer extends React.Component {
         <div data-vjs-player style={{ margin: "0px auto" }}>
           <video ref={node => this.videoNode = node} className="video-js"></video>
         </div>
-        <div style={{ textAlign: 'center' }} onClick={this.screenshot.bind(this)}>截屏</div>
-        <canvas id="videoCanvasBox"></canvas>
+        {/* <div style={{ textAlign: 'center' }} onClick={this.screenshot.bind(this)}>截屏</div> */}
+        {/* <canvas id="videoCanvasBox"></canvas> */}
       </div>
     )
   }
